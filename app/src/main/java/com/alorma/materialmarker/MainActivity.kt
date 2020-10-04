67d7b5eeef0b05809d8ce3c95f304e26798cb415
Bernat Borrás <bernat.borras@adevinta.com>
2020-10-04 21:26:29 +0200

    Add color override from styles

--- app/src/main/java/com/alorma/materialmarker/MainActivity.kt
+++ app/src/main/java/com/alorma/materialmarker/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.alorma.materialmarker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        marker0.setOnClickListener { }
+        marker1.setOnClickListener { }
+        marker2.setOnClickListener { }
+    }
 }
--- app/src/main/java/com/alorma/materialmarker/MaterialMapMarker.kt
+++ app/src/main/java/com/alorma/materialmarker/MaterialMapMarker.kt
@@ -2,10 +2,15 @@ package com.alorma.materialmarker
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.InsetDrawable
+import android.graphics.drawable.RippleDrawable
 import android.util.AttributeSet
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.withStyledAttributes
 import androidx.core.view.ViewCompat
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.shape.MaterialShapeDrawable
@@ -21,17 +26,49 @@ class MaterialMapMarker @JvmOverloads constructor(
 ) : AppCompatTextView(context, attributeSet, defStyleAttr) {
 
     private var strokeWidth: Float = 2f
+
+    private var pointSize: Int = 8.dp
+
     private var strokeColor: ColorStateList = ColorStateList.valueOf(
-        MaterialColors.getColor(this, R.attr.colorOnPrimary)
+        MaterialColors.getColor(this, R.attr.colorOnSurface)
     )
 
-    private val fillColor: ColorStateList = ColorStateList.valueOf(
-        MaterialColors.getColor(this, R.attr.colorPrimary)
+    private var fillColor: ColorStateList = ColorStateList.valueOf(
+        MaterialColors.getColor(this, R.attr.colorSurface)
+    )
+
+    private var rippleColor: ColorStateList = ColorStateList.valueOf(
+        MaterialColors.getColor(this, R.attr.colorOnPrimary)
     )
 
     init {
         setTextColor(strokeColor)
-        initBackground(context, attributeSet, defStyleAttr, defStyleRes)
+        context.withStyledAttributes(
+            attributeSet,
+            R.styleable.MaterialMapMarker,
+            defStyleAttr,
+            defStyleRes
+        ) {
+            readAttributes()
+            initBackground(context, attributeSet, defStyleAttr, defStyleRes)
+        }
+    }
+
+    private fun TypedArray.readAttributes() {
+        pointSize = getDimensionPixelOffset(R.styleable.MaterialMapMarker_pointSize, pointSize)
+
+
+        rippleColor = getColorStateList(
+            R.styleable.MaterialMapMarker_rippleColor
+        ) ?: rippleColor
+
+        fillColor = getColorStateList(
+            R.styleable.MaterialMapMarker_fillColor
+        ) ?: fillColor
+
+        strokeColor = getColorStateList(
+            R.styleable.MaterialMapMarker_strokeColor
+        ) ?: strokeColor
     }
 
     private fun initBackground(
@@ -40,25 +77,40 @@ class MaterialMapMarker @JvmOverloads constructor(
         defStyleAttr: Int,
         defStyleRes: Int
     ) {
+
+        val pointEdge = TriangleEdgeTreatment(pointSize.toFloat(), false)
         val shapeAppearanceModel = ShapeAppearanceModel.builder(
             context,
             attributeSet,
             defStyleAttr,
             defStyleRes
-        ).setBottomEdge(TriangleEdgeTreatment(12.dp.toFloat(), false))
+        ).setBottomEdge(pointEdge)
             .build()
 
         val materialDrawable = MaterialShapeDrawable(shapeAppearanceModel)
 
         materialDrawable.initializeElevationOverlay(context)
-        materialDrawable.elevation = ViewCompat.getElevation(this)
+        materialDrawable.elevation = ViewCompat.getElevation(this@MaterialMapMarker)
 
         materialDrawable.fillColor = fillColor
 
         materialDrawable.strokeWidth = strokeWidth
         materialDrawable.strokeColor = strokeColor
 
-        ViewCompat.setBackground(this, materialDrawable)
+
+        val maskDrawable = MaterialShapeDrawable(shapeAppearanceModel)
+
+        val rippleDrawable = RippleDrawable(
+            rippleColor,
+            wrapDrawableWithInset(materialDrawable),
+            maskDrawable
+        )
+
+        ViewCompat.setBackground(this@MaterialMapMarker, rippleDrawable)
+    }
+
+    private fun wrapDrawableWithInset(drawable: Drawable): InsetDrawable {
+        return InsetDrawable(drawable, 0.dp, 0.dp, 0.dp, pointSize)
     }
 
     override fun setElevation(elevation: Float) {
